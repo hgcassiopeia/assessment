fd6be3af887c6c6a1513030361c30e49d1a6663d
hgcassiopeia <ph.thitikarn_st@tni.ac.th>
2022-12-21 18:56:05 +0700

    feat: add go module and echo

--- server.go
+++ server.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":2565"))
 }
